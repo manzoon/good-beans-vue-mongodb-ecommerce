1102973bdf2efe3918858c255717495b0731d92f
KoduKodu <kubrakradoslaw@gmail.com>
2020-06-23 01:08:56 +0200

    basic vue setup - vuex and router installed

---
+++ client/src/components/HomeCategoriesNavigation.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Categories Nav</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ client/src/pages/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div id="main-header">
+      <HomeHeader />
+      <HomeCategoriesNavigation />
+      <HomeProducts />
+      <HomeFooter />
+      <router-link to="/">Home</router-link>
+    </div>
+    <!-- <router-view /> -->
+  </div>
+</template>
+
+<script>
+import HomeHeader from "../components/HomeHeader";
+import HomeCategoriesNavigation from "../components/HomeCategoriesNavigation";
+import HomeProducts from "../components/HomeProducts";
+import HomeFooter from "../components/HomeFooter";
+
+export default {
+  name: "Home",
+  components: {
+    HomeHeader,
+    HomeCategoriesNavigation,
+    HomeProducts,
+    HomeFooter
+  }
+};
+</script>
+
+<style>
+</style>
